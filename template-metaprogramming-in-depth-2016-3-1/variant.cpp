7be8e09a2e9ed7030635d7bcc6de19770ee21e4b
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-03-15 17:22:26 -0500

    add normal template

--- template-metaprogramming-in-depth-2016-3-1/variant.cpp
+++ template-metaprogramming-in-depth-2016-3-1/variant.cpp
@@ -34,15 +34,44 @@ inline variant_overload_set<T, Args...> variant_overload(Args &&... args)
   return variant_overload_set<T, Args...>(args...);
 }
 
+struct normal_visitor : public boost::variant<int> {
+  int operator()() const
+  {
+    return 0;
+  }
+
+  int operator()(int i) const
+  {
+    return i + 1;
+  }
+
+  int operator()(const char * s) const
+  {
+    return strlen(s);
+  }
+
+  /* we can do this here, but not with lambdas */
+  template <typename T> int operator()(std::stack<T> s) const
+  {
+    return s.size();
+  }
+
+  template <typename T> int operator()(T) const
+  {
+    return 34;
+  }
+};
+
 int main()
 {
-  auto overloaded_funs = overload(
-      []() { return 0; }, [](int i) { return i + 1; },
-      [](const char * s) { return strlen(s); },
-      /* can't be stack<T>! must be concrete type */
-      [](std::stack<int> s) { return s.size(); },
-      /* can't have multiple "auto" matchers, the standard says no */
-      [](auto) { return 34; });
+  auto overloaded_funs =
+      overload([]() { return 0; }, [](int i) { return i + 1; },
+               [](const char * s) { return strlen(s); },
+               /* can't be stack<T>! must be concrete type */
+               [](std::stack<int> s) { return s.size(); },
+               /* can't have multiple "auto" matchers, the standard says no */
+               [](auto) { return 34; });
+  /* normal_visitor overloaded_funs; */
   std::stack<int> s;
   s.push(3);
   std::vector<int> results;
@@ -60,6 +89,7 @@ int main()
                             [](auto a) { return a.size(); });
 
   boost::variant<int, std::string> v("hello");
-  int res = boost::apply_visitor(overload_v, v);
+  /* int res = boost::apply_visitor(overload_v, v); */
+  int res = boost::apply_visitor(normal_visitor(), v);
   std::cout << res << std::endl;
 }
